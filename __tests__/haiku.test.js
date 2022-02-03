dcd456480a3dfada69d9ec29de45b20c1a4b98c5
wcjameson <williamjameson90@gmail.com>
2022-02-03 14:49:32 -0800

    WIP fixing testing errors, function undefined issues

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -12,33 +12,38 @@ describe('Haiku', () => {
 
   test('should find and return the number of syllables in the first string', () => {
     const haiku = new Haiku("line one", "line two", "line three");
-    expect(haiku.syllableCountLine1()).toEqual(4);
+    expect(haiku.stringSyllableCount1()).toEqual(2);
     
   });
 
   test('should find and return the number of syllables in the second string', () => {
     const haiku = new Haiku("line one", "line two", "line three");
-    expect(haiku.syllableCountLine2()).toEqual(3);
+    expect(haiku.stringSyllableCount2()).toEqual(3);
   });
 
   test('should find and return the number of syllables in the third string', () => {
     const haiku = new Haiku("line one", "line two", "line three");
-    expect(haiku.syllableCountLine3()).toEqual(3);
+    expect(haiku.stringSyllableCount3()).toEqual(3);
   });
 
   test('should find and return number of syllables in first string regardless of case', () => {
     const haiku = new Haiku("LINE ONe", "LIne TwO", "LINE Three");
-    expect(haiku.syllableCountLine1()).toEqual(4);
+    expect(haiku.stringSyllableCount1()).toEqual(4);
   });
 
   test('should find and return number of syllables in second string regardless of case', () => {
     const haiku = new Haiku("LINE ONe", "LIne TwO", "LINE Three");
-    expect(haiku.syllableCountLine2()).toEqual(3);
+    expect(haiku.stringSyllableCount2()).toEqual(3);
   });
 
   test('should find and return number of syllables in third string regardless of case', () => {
     const haiku = new Haiku("LINE ONe", "LIne TwO", "LINE Three");
-    expect(haiku.syllableCountLine3()).toEqual(3);
+    expect(haiku.stringSyllableCount3()).toEqual(3);
   });
+
+  // test('should find and return number of syllables, making exceptions for es || ed at end of word', () => {
+  //   const haiku = new Haiku("Does that worked", "Maybe it works",  "Hoped it works");
+  //   expect(haiku.syllableCountLine1()).toEqual(3);
+  // });
   
 });
--- src/haiku.js
+++ src/haiku.js
@@ -4,26 +4,60 @@ export default class Haiku {
     this.line2 = line2;
     this.line3 = line3;
   }
-
-  syllableCountLine1(str) {
-    str = this.line1;
-    str = str.toLowerCase();
-    let syllableCount1 = str.match(/[aeiouy]{1,2}/g).length;
-    return syllableCount1;
+  
+  syllableCount(word) {
+    word = word.toLowerCase();
+    if (word.length <= 3) { return 1; }
+    word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
+    word = word.replace(/^y/, '');
+    let sylCount = word.match(/[aeiouy]{1,2}/g).length;
+    // if(sylCount)
+    
+    return sylCount.length;
   }
   
-  syllableCountLine2(str) {
-    str = this.line2;
-    str = str.toLowerCase(); 
-    const syllableCount2 = str.match(/[aeiouy]{1,2}/g).length;
-    return syllableCount2;
+  
+  stringSyllableCount1(string) {
+    string = this.line1;
+    let wordArray = string.split(" ");
+    let numberOfSyllables = 0;
+    wordArray.forEach(function(element) {
+      numberOfSyllables += syllableCount(element);
+    });
+    return numberOfSyllables;
   }
 
-  syllableCountLine3(str) {
-    str = this.line3;
-    str = str.toLowerCase(); 
-    const syllableCount3 = str.match(/[aeiouy]{1,2}/g).length;
-    return syllableCount3;
+  stringSyllableCount2(string) {
+    let wordArray = string.split(" ");
+    let numberOfSyllables = 0;
+    wordArray.forEach(function(element) {
+      numberOfSyllables += syllableCount(element);
+    });
+    return numberOfSyllables;
   }
+
+  stringSyllableCount3(string) {
+    let wordArray = string.split(" ");
+    let numberOfSyllables = 0;
+    wordArray.forEach(function(element) {
+      numberOfSyllables += syllableCount(element);
+    });
+    return numberOfSyllables;
+  }
+
+  
+  // syllableCountLine2(str) {
+  //   str = this.line2;
+  //   str = str.toLowerCase(); 
+  //   const syllableCount2 = str.match(/[aeiouy]{1,2}/g).length;
+  //   return syllableCount2;
+  // }
+
+  // syllableCountLine3(str) {
+  //   str = this.line3;
+  //   str = str.toLowerCase(); 
+  //   const syllableCount3 = str.match(/[aeiouy]{1,2}/g).length;
+  //   return syllableCount3;
+  // }
 
 }
